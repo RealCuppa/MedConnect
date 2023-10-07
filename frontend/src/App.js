c2f243a9647ae3c3ab4b0457da4c61a7b404f0b8
Atharv <atharvakumar0@gmail.com>
2023-10-07 15:32:51 +0530

    made some few changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import {Route, Routes} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 import Login from './components/Login/Login';
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      {/*<Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
+      <Route path="/about" element={<About />} />
+      {/*<Route path="/services" element={<Services />} />
       <Route path="/contact" element={<Contact />} />*/}
       <Route path="/login" element={<Login />} />
       {/*<Route path="/register" element={<Register />} />
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return(
     <div className="flex flex-col justify-center items-center w-screen min-h-screen bg-orange-100 mt-12">
       <div className="flex flex-col justify-center items-center w-4/5 md: 3/4 h-full">
-        <h1 className="text-4xl md:text-5xl mb-4 text-center">Get your medical record from your <span>House</span></h1>
+        <h1 className="text-4xl font-bold md:text-5xl mb-4 text-center">Get your medical record from your <span>House</span></h1>
         <p className="text-xl md:text-2xl mb-4 text-center">All reports on your upcoming treatment on one platform</p>
         <Button className="text-lg md:text-xl bg-[#ffb800] hover:bg-[#ffb800cc] text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline" label="Get Started" onclick={() => navigate("/register")}></Button>
       </div>
