7b8fd6628a568bf6a1077fbee6346d05ac126f45
Atharv <atharvakumar0@gmail.com>
2023-10-07 08:40:08 +0530

    Added Routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-    MedConnect
-    </div>
+    <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/*<Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />*/}
+      <Route path="/login" element={<Login />} />
+      {/*<Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />*/}
+    </Routes>
+    </>
   );
 }
 
